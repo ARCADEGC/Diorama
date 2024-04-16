00e62efc98f06502e63fc4066cf2e807d436f280
Jaromír Trnka <JrTrnka55@gmail.com>
2024-04-16 19:12:41 +0200

    fixed development server errors

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,19 +1,9 @@
 "use client";
 
-import { zoomies } from "ldrs";
-
 export default function Loading() {
-    zoomies.register();
-
     return (
         <div className="absolute left-0 top-0 grid h-screen w-screen place-content-center bg-background">
-            <l-zoomies
-                size="150"
-                stroke="2"
-                bg-opacity="0.1"
-                speed="1.6"
-                color="white"
-            />
+            <div className="after:animate-ldrs-zoomies relative flex h-px w-40 items-center justify-center overflow-hidden rounded-full bg-background [transform:translate3d(0,0,0)] before:absolute before:left-0 before:top-0 before:size-full before:bg-foreground before:opacity-10 before:content-[''] after:size-full after:rounded-full after:bg-foreground after:transition-colors after:content-['']"></div>
         </div>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,11 +77,20 @@ const config = {
                         backgroundPosition: "350% 50%, 350% 50%",
                     },
                 },
+                "ldrs-zoomies": {
+                    from: {
+                        transform: "translateX(-100%)",
+                    },
+                    to: {
+                        transform: "translateX(100%)",
+                    },
+                },
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
                 aurora: "aurora 60s linear infinite",
+                "ldrs-zoomies": "ldrs-zoomies 1.4s ease infinite",
             },
             fontFamily: {
                 serif: "'Satoshi', sans-serif",
